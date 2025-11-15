7d21d629bd2805eeeed0c06ecac63da3bf4c30d3
William <pedro.williamsoares2022@gmail.com>
2025-11-15 20:23:22 -0300

    Initial commit

---
+++ src/main/java/com/minha/prateleira/prateleira_app/PrateleiraAppApplication.java
@@ -0,0 +1,13 @@
+package com.minha.prateleira.prateleira_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrateleiraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrateleiraAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minha/prateleira/prateleira_app/PrateleiraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minha.prateleira.prateleira_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrateleiraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
